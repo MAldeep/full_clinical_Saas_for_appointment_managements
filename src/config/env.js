7190f8ed65fcf6db4f36f0c8e9a08f9e421259c6
MAldeep <m7mdeldeeeep@gmail.com>
2026-08-08 15:47:30 +0300

    step_03: Config folder for env validation schema

---
+++ src/config/env.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+import z from "zod";
+
+dotenv.config();
+
+const envSchema = z.object({
+  PORT: z
+    .string()
+    .transform((val) => parseInt(val, 10))
+    .default("5000"),
+  MONGODB_URI: z.string().url("MONGODB_URI must be a valid connection string"),
+  NODE_ENV: z.enum(["development", "production"]).default("development"),
+  JWT_SECRET: z.string().min(10),
+  JWT_EXPIRES_IN: z.string(),
+});
+
+const parseResult = envSchema.safeParse(process.env);
+
+if (!parseResult.success) {
+  console.error("Invalid environment variables");
+  console.error(parseResult.error.format());
+  process.exit(1);
+}
+
+export const env = parseResult.data;
