ad4c08369dbc856ab947439bb2d6ba20e41bbb11
MAldeep <m7mdeldeeeep@gmail.com>
2026-08-08 16:52:27 +0300

    step_08 : services for auth and appointments

---
+++ src/services/appointment.services.js
@@ -0,0 +1,40 @@
+import { Appointment } from "../models/appoitment.model.js";
+
+export const createAppointmentService = async (appointmentData) => {
+  const newAppointment = await Appointment.create(appointmentData);
+  return newAppointment;
+};
+
+export const getAllAppointmentsService = async (filter) => {
+  const appointments = await Appointment.find(filter).populate(
+    "doctor",
+    "name email role",
+  );
+  return appointments;
+};
+
+export const getAppointmentByIdService = async (id) => {
+  const appoitment = await Appointment.findById(id).populate(
+    "doctor",
+    "name email role",
+  );
+  if (!appoitment) return null;
+  return appoitment;
+};
+
+export const updateAppointmentService = async (id, updateData) => {
+  const updatedAppointment = await Appointment.findByIdAndUpdate(
+    id,
+    updateData,
+    {
+      new: true,
+      runValidators: true,
+    },
+  );
+  return updatedAppointment;
+};
+
+export const deleteAppointmentService = async (id) => {
+  const deletedAppointment = await Appointment.findByIdAndDelete(id);
+  return deletedAppointment;
+};
---
+++ src/services/auth.services.js
@@ -0,0 +1,26 @@
+import { User } from "../models/user.model.js";
+import AppError from "../utils/appError.js";
+import { generateToken } from "../utils/generateToken.js";
+
+export const registerUser = async (userData) => {
+  const existingUser = await User.findOne({ email: userData.email });
+  if (existingUser) {
+    throw new AppError("User with this email is already exists", 400);
+  }
+  const newUser = await User.create(userData);
+  newUser.password = undefined;
+  return newUser;
+};
+
+export const loginUser = async (email, password) => {
+  if (!email || !password) {
+    throw new AppError("Please provide email and password", 400);
+  }
+  const user = await User.findOne({ email }).select("+password");
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    throw new AppError("Incorrect email or password", 401);
+  }
+  const token = generateToken(user._id);
+  user.password = undefined;
+  return { user, token };
+};
