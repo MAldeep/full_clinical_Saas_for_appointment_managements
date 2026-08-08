50534d12ac99537e196abae5de7ff366b1b22543
MAldeep <m7mdeldeeeep@gmail.com>
2026-08-08 17:24:11 +0300

    step_09 : controllers including errorController, auth , appointments

---
+++ src/controllers/appointment.controllers.js
@@ -0,0 +1,105 @@
+import {
+  createAppointmentService,
+  deleteAppointmentService,
+  getAllAppointmentsService,
+  getAppointmentByIdService,
+  updateAppointmentService,
+} from "../services/appointment.services.js";
+import AppError from "../utils/appError.js";
+import catchAsync from "../utils/catchAsync.js";
+
+export const createAppointmentCtrl = catchAsync(async (req, res, next) => {
+  const appointmentData = req.body;
+  const newAppointment = await createAppointmentService(appointmentData);
+
+  res.status(201).json({
+    success: true,
+    data: newAppointment,
+  });
+});
+
+export const getAllAppointmentsCtrl = catchAsync(async (req, res, next) => {
+  const role = req.user;
+  let filter;
+  if (role === "Doctor") {
+    filter = { doctor: req.user._id };
+  } else {
+    filter = {};
+  }
+  const appointments = await getAllAppointmentsService(filter);
+
+  res.status(200).json({
+    success: true,
+    results: appointments.length,
+    data: appointments,
+  });
+});
+
+export const getAppointmentByIdCtrl = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const appointment = await getAppointmentByIdService(id);
+  if (!appointment) {
+    return next(new AppError("Appointment not found", 404));
+  }
+  const isAdmin = req.user.role === "Admin";
+  const isOwner = appointment.doctor._id.toString() === req.user._id.toString();
+  if (!isAdmin && !isOwner) {
+    return next(
+      new AppError(
+        "You do not have permission to access this appointment",
+        403,
+      ),
+    );
+  }
+  res.status(200).json({
+    success: true,
+    data: appointment,
+  });
+});
+
+export const updateAppointmentCtrl = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const updateData = req.body;
+  const updatedAppointment = await updateAppointmentService(id, updateData);
+  if (!updatedAppointment) {
+    return next(new AppError("Appointment not found", 404));
+  }
+  const isAdmin = req.user.role === "Admin";
+  const isOwner =
+    updatedAppointment.doctor._id.toString() === req.user._id.toString();
+  if (!isAdmin && !isOwner) {
+    return next(
+      new AppError(
+        "You do not have permission to access this appointment",
+        403,
+      ),
+    );
+  }
+  res.status(200).json({
+    success: true,
+    data: updatedAppointment,
+  });
+});
+
+export const deleteAppointmentCtrl = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const deletedAppointment = await deleteAppointmentService(id);
+  if (!deletedAppointment) {
+    return next(new AppError("Appointment not found", 404));
+  }
+  const isAdmin = req.user.role === "Admin";
+  const isOwner =
+    deletedAppointment.doctor._id.toString() === req.user._id.toString();
+  if (!isAdmin && !isOwner) {
+    return next(
+      new AppError(
+        "You do not have permission to access this appointment",
+        403,
+      ),
+    );
+  }
+  res.status(200).json({
+    success: true,
+    data: deletedAppointment,
+  });
+});
---
+++ src/controllers/auth.controllers.js
@@ -0,0 +1,23 @@
+import { loginUser, registerUser } from "../services/auth.services.js";
+import catchAsync from "../utils/catchAsync.js";
+
+export const registerUserCtrl = catchAsync(async (req, res, next) => {
+  const userData = req.body;
+  const newUser = await registerUser(userData);
+  res.status(201).json({
+    success: true,
+    data: newUser,
+  });
+});
+
+export const loginUserCtrl = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  const { user, token } = await loginUser(email, password);
+  res.status(200).json({
+    success: true,
+    token,
+    data: {
+      user,
+    },
+  });
+});
